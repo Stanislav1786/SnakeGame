67194421460063aa518fa5b76ea53b3bc49f6278
Stanislav1786 <ogonek1786@gmail.com>
2020-04-13 12:12:53 +0300

    Создал файл snake.js для удобства хранения функций

--- js/script.js
+++ js/script.js
@@ -6,17 +6,33 @@ canvas.width = 500
 canvas.height = 500
 
 
-drawRect({
+const param = {
     x: 10,
-    y: 10,
+    y: 100,
     width: 50,
     height: 50,
     fillColor: "green"
-})
+}
+
+drawRect(param)
+
+requestAnimationFrame(loop)
 
 function drawRect(param) {
     context.beginPath()
     context.rect(param.x, param.y, param.width, param.height)
     context.fillStyle = param.fillColor
     context.fill()
+}
+
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function loop(timestamp) {
+    requestAnimationFrame(loop)
+
+    clearCanvas()
+
+    drawRect(param)
 }
